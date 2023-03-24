33062b91cff4195edc1cce814be6aa784b3146ae
Evalyne306 <evalyne.wambui@student.moringaschool.com>
2023-03-24 21:16:39 +0300

    done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   
   def index
-    byebug
+    
     session[:session_hello] ||= "Hello"
     cookies[:cookies_hello] ||= "Hello"
     render json: { session: session, cookies: cookies.to_hash }
